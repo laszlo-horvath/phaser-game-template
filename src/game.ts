c2c3c7b0773ecfbcf38bc56094b27d6f0496800f
RynoRn <horvathlaszlo88@gmail.com>
2019-02-07 12:47:16 +0100

    Updating the game creation.

---
+++ src/game.ts
@@ -0,0 +1,36 @@
+
+import { BootScene, PreloadScene, MenuScene, GameScene } from './scenes';
+
+const config: GameConfig = {
+  title: 'Your Game',
+  version: '1.0',
+
+  // Canvas size
+  width: 800,
+  height: 600,
+
+  // Automatically detect supported rendering engine to use (e.g. Canvas, WebGL)
+  type: Phaser.AUTO,
+
+   // DOM element to render the game into
+  parent: 'game-container',
+
+  // Scenes
+  scene: [
+    BootScene,
+    PreloadScene,
+    MenuScene,
+    GameScene
+  ]
+};
+
+export class Game extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
+
+window.addEventListener('load', () => {
+  const game = new Game(config);
+  console.log('Game created.', game);
+});
