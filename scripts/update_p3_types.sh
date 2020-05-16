8035b52f942c8cdd31064a5ce58746ade5c00e52
lhorvath <lhorvath@cloudera.com>
2020-05-16 18:00:18 +0200

    Updating typings file.

--- scripts/update_p3_types.sh
+++ scripts/update_p3_types.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-REMOTEURL='https://raw.githubusercontent.com/photonstorm/phaser3-docs/master/typescript/phaser.d.ts'
+# phaser type defition
+REMOTEURL='https://raw.githubusercontent.com/photonstorm/phaser/v3.23.0/types/phaser.d.ts'
+# Please note:
+# The reference types="./matter" may need to be updated to path="./matter" inside phaser.d.ts.
 DEST=src/typings/phaser.d.ts
+
+curl $REMOTEURL > $DEST
+
+# matter js type defition
+REMOTEURL='https://raw.githubusercontent.com/photonstorm/phaser/v3.23.0/types/matter.d.ts'
+DEST=src/typings/matter.d.ts
 
 curl $REMOTEURL > $DEST
